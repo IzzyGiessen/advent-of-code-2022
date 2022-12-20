7f97ffddb56aecaa62cef0bc98d2b8f44b68ffad
Izzy van der Giessen <i.vandergiessen@ziggo.nl>
2022-12-20 18:19:42 +0100

    Finish day 3

--- day3/Main.java
+++ day3/Main.java
@@ -1,9 +1,12 @@
+import java.util.List;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -21,23 +24,29 @@ public class Main {
     String line = br.readLine();
 
     int sum = 0;
+    int counter = 0;
+    List<Set<Character>> sets = new ArrayList<>();
     do {
-      int length = line.length();
       Set<Character> set = new HashSet<>();
-      for (int i = 0; i < length/2; i++) {
-        set.add(line.charAt(i));
+      for (char c : line.toCharArray()) {
+        set.add(c);
       }
-      for (int i = length/2; i < length; i++) {
-        if (set.contains(line.charAt(i))) {
-          char c = line.charAt(i);
-          if (c - 'A' < 26) {
-            sum += c - 'A' + 27;
-          } else {
-            sum += c - 'a' + 1;
-          }
-          break;
+      sets.add(set);
+
+      if (++counter % 3 == 0) {
+        Set<Character> s = sets.get(0);
+        for (int i = 1; i < sets.size(); i++) {
+          s.retainAll(sets.get(i));
         }
+        char c = s.iterator().next();
+        if (c - 'A' < 26) {
+          sum += c - 'A' + 27;
+        } else {
+          sum += c - 'a' + 1;
+        }
+        sets.clear();
       }
+
     } while ((line = br.readLine()) != null);
 
     System.out.println(sum);
